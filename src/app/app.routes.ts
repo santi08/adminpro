284349097c7a7c9a0c2d52e62f19fbf9c4bfac80
Santiago Guarin <santiaristi_1994@hotmail.com>
2019-09-19 23:20:59 -0500

    Primer Commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,29 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register.component';
+
+const appRoutes: Routes = [
+    {
+        path: '',
+        component: PagesComponent,
+        //Las rutas que van a funcionar con el router-outlet secundario
+        children: [
+            { path: 'dashboard', component: DashboardComponent },
+            { path: 'graficas1', component: Graficas1Component },            
+            { path: 'progress', component: ProgressComponent },
+            { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+        ]
+    },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    
+    { path: '**', component: NopagefoundComponent },
+];
+
+export const APP_ROUTES = RouterModule.forRoot(appRoutes, { useHash: true });
