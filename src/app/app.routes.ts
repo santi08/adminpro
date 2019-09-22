75ba93dbab385a44668ee01adcf36cd6068f7600
Santiago Guarin <santiaristi_1994@hotmail.com>
2019-09-22 00:04:18 -0500

    Modificaciones en los modulos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,28 +1,23 @@
 import { RouterModule, Routes } from '@angular/router';
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
 const appRoutes: Routes = [
-    {
-        path: '',
-        component: PagesComponent,
-        //Las rutas que van a funcionar con el router-outlet secundario
-        children: [
-            { path: 'dashboard', component: DashboardComponent },
-            { path: 'graficas1', component: Graficas1Component },            
-            { path: 'progress', component: ProgressComponent },
-            { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-        ]
-    },
+    // {
+    //     path: '',
+    //     component: PagesComponent,
+    //     //Las rutas que van a funcionar con el router-outlet secundario
+    //     children: [
+    //         { path: 'dashboard', component: DashboardComponent },
+    //         { path: 'graficas1', component: Graficas1Component },            
+    //         { path: 'progress', component: ProgressComponent },
+    //         { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    //     ]
+    // },
     { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent },
-    
+    { path: 'register', component: RegisterComponent },    
     { path: '**', component: NopagefoundComponent },
 ];
 
